7e30379ae38a75391837f097acc8e6eee65cd0b8
Alex <64327268+AlexDeLos@users.noreply.github.com>
2026-01-31 17:34:47 +0100

    Update RNA_seq_processing.py

--- src/data_importing/RNA_seq_processing.py
+++ src/data_importing/RNA_seq_processing.py
@@ -319,7 +319,7 @@ def download_experiments_RNA_seq_nf_core(gse_list:list[str],root_storage_dir:str
     PATH_TO_INDEX = f"{root_storage_dir}genome_index/tair10"
     PATH_TO_GTF = f"{root_storage_dir}genome_index/Arabidopsis_thaliana.TAIR10.56.gtf"
     
-    processor = RNASeq_processor(threads=1, genome_index=PATH_TO_INDEX, gtf_annotation=PATH_TO_GTF)
+    processor = RNASeq_processor(threads=1, genome_index=PATH_TO_INDEX, gtf_annotation=PATH_TO_GTF,profile='singularity')
     tracker_save_path = os.path.join(output_dir, "rnaseq_tracker_stats.json")
     
     if not os.path.exists(output_dir): os.makedirs(output_dir)
