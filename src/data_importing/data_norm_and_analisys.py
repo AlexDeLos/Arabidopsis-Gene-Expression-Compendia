28c76cafe1bd5fb1a336702a878d0534f99a2d9d
Alex De Los Santos Subirats <A.DeLosSantosSubirats@tudelft.nl>
2026-05-20 10:31:56 +0200

    Update data_norm_and_analisys.py

--- src/data_importing/data_norm_and_analisys.py
+++ src/data_importing/data_norm_and_analisys.py
@@ -44,12 +44,14 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import warnings
+from scipy.sparse.linalg import svds
+from sklearn.decomposition import TruncatedSVD
+
 # --- R integration ---
 import rpy2.robjects as ro
 from rpy2.robjects import numpy2ri, pandas2ri
 from rpy2.robjects.conversion import localconverter
 from rpy2.robjects.packages import importr
-from sklearn.decomposition import TruncatedSVD
 
 module_dir = "./"
 sys.path.append(module_dir)
